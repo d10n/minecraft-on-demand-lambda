e761c01db81cf1259b307c7b2d4ddc237e31f287
aqemery <aqemery@gmail.com>
2017-06-08 13:32:42 -0400

    updating tfscript and server sync

--- minecraft.sh
+++ minecraft.sh
@@ -3,4 +3,5 @@ yum update -y
 mkdir MinecraftServer
 aws s3 sync s3://mc-world-backup MinecraftServer/
 cd MinecraftServer
+crontab -l | { cat; echo "*/5 * * * * aws s3 sync /MinecraftServer/ s3://mc-world-backup"; } | crontab -
 sudo java -Xmx1G -Xms1G -jar minecraft_server.1.11.2.jar nogui
