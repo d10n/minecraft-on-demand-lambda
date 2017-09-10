8f70e2fbc835607f30e4885df16a9f583fafc406
aqemery <aqemery@gmail.com>
2017-09-10 08:51:58 -0400

    1.12 update

--- minecraft.sh
+++ minecraft.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 yum update -y
+yum install java-1.8.0 -y
+yum remove java-1.7.0-openjdk -y
 mkdir MinecraftServer
 aws s3 sync s3://mc-world-backup MinecraftServer/
 cd MinecraftServer
+pip install mcstatus
 crontab -l | { cat; echo "*/5 * * * * aws s3 sync /MinecraftServer/ s3://mc-world-backup"; } | crontab -
-sudo java -Xmx1G -Xms1G -jar minecraft_server.1.11.2.jar nogui
+crontab -l | { cat; echo "*/5 * * * * python /MinecraftServer/auto_shutoff.py"; } | crontab -
+sudo java -Xmx1G -Xms1G -jar minecraft_server.1.12.jar nogui
