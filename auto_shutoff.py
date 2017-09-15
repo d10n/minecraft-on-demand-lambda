d1385c43d01f57ea3a541a53beda76523d252c67
d10n <david@bitinvert.com>
2017-09-15 16:54:12 -0400

    Replace tabs with spaces

--- auto_shutoff.py
+++ auto_shutoff.py
@@ -9,18 +9,17 @@ server = MinecraftServer.lookup("localhost:25565")
 status = server.status()
 
 if os.path.exists('/last_activity'):
-	f = open('/last_activity', 'r+')
+    f = open('/last_activity', 'r+')
 
-	if status.players.online:
-		f.seek(0)
-		f.write(str(time.time()))
-		f.truncate()
-	else:
-		old_time = float(f.read()) 
-		time_past = time.time() - old_time
-		if time_past > (30*60):
-			req = urllib2.urlopen('<SERVER DESTROY LAMBDA FUNCTION>')
+    if status.players.online:
+        f.seek(0)
+        f.write(str(time.time()))
+        f.truncate()
+    else:
+        old_time = float(f.read())
+        time_past = time.time() - old_time
+        if time_past > (30 * 60):
+            req = urllib2.urlopen('<SERVER DESTROY LAMBDA FUNCTION>')
 else:
-	f = open('/last_activity', 'w')
-	f.write(str(time.time()))
-	
+    f = open('/last_activity', 'w')
+    f.write(str(time.time()))
