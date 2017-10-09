7887d660c4ffffcf8ec72784cbdc2f2294734341
d10n <david@bitinvert.com>
2017-10-09 10:59:54 -0400

    Comment cloudfront

--- core/core.tf
+++ core/core.tf
@@ -96,51 +96,51 @@ resource "aws_s3_bucket_object" "dashboard_index" {
 #  etag         = "${md5(data.template_file.dashboard_error.rendered)}"
 #}
 
-resource "aws_cloudfront_distribution" "cdn" {
-  origin {
-    origin_id   = "${var.aws_s3_server_dashboard}"
-    domain_name = "${aws_s3_bucket.minecraft_server_dashboard.bucket_domain_name}"
-  }
-
-  # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
-  #aliases = ["${var.domain}"]
-
-  enabled             = true
-  default_root_object = "index.html"
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.aws_s3_server_dashboard}"
-
-    forwarded_values {
-      query_string = true
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  # The cheapest priceclass
-  #price_class = "PriceClass_100"
-
-  # This is required to be specified even if it's not used.
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      locations        = []
-    }
-  }
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
+#resource "aws_cloudfront_distribution" "cdn" {
+#  origin {
+#    origin_id   = "${var.aws_s3_server_dashboard}"
+#    domain_name = "${aws_s3_bucket.minecraft_server_dashboard.bucket_domain_name}"
+#  }
+#
+#  # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
+#  #aliases = ["${var.domain}"]
+#
+#  enabled             = true
+#  default_root_object = "index.html"
+#  default_cache_behavior {
+#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id = "${var.aws_s3_server_dashboard}"
+#
+#    forwarded_values {
+#      query_string = true
+#
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    viewer_protocol_policy = "redirect-to-https"
+#    compress               = true
+#    min_ttl                = 0
+#    default_ttl            = 3600
+#    max_ttl                = 86400
+#  }
+#
+#  # The cheapest priceclass
+#  #price_class = "PriceClass_100"
+#
+#  # This is required to be specified even if it's not used.
+#  restrictions {
+#    geo_restriction {
+#      restriction_type = "none"
+#      locations        = []
+#    }
+#  }
+#  viewer_certificate {
+#    cloudfront_default_certificate = true
+#  }
+#}
 
 resource "aws_s3_bucket_object" "auto_shutoff" {
   bucket = "${aws_s3_bucket.minecraft_world_backup.id}"
